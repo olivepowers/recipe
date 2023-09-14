67c88bcecb7ea65f116f1944d69053bba4f0edcd
olivepowers <powersog@gmail.com>
2023-09-14 16:13:33 -0400

    render recipes on homepage

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,17 +1,42 @@
 import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  Text,
+} from "@radix-ui/themes";
 
 export type RecipeProps = {
-    id: number;
-    title: string;
-    description: string;
-    author: {
-        name: string;
-        email: string;
-    } | null;
+  id: number;
+  title: string;
+  description: string;
+  author: {
+    name: string;
+    email: string;
+  } | null;
 };
 
-const Recipe: React.FC<{ Recipe: RecipeProps }> = ({ Recipe }) => {
-    return <div>{JSON.stringify(Recipe, null, 2)}</div>
+const Recipe: React.FC<{ recipe: RecipeProps }> = ({ recipe }) => {
+  return (
+    <Card size="2">
+      <Flex gap="5" align="center">
+        <Box>
+          <Text as="div" size="4" weight="bold">
+            {recipe.title}
+          </Text>
+          <Text as="div" size="2" color="gray">
+            {recipe.author?.name}
+          </Text>
+          <Text as="div" size="3">
+            {recipe.description}
+          </Text>
+        </Box>
+      </Flex>
+    </Card>
+  );
 };
 
 export default Recipe;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,40 @@
-
-import { Box, Button, Card, Container, Flex, Heading, Text } from "@radix-ui/themes";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  Text,
+} from "@radix-ui/themes";
 import type { GetServerSideProps } from "next";
 import prisma from "../lib/prisma";
 import { RecipeProps } from "@web/components/Recipe";
+import Recipe from "@web/components/Recipe";
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const recipes = await prisma.recipe.findMany();
+  const recipes = await prisma.recipe.findMany({
+    include: {
+      author: true,
+    },
+  });
   return {
     props: { recipes },
   };
-}
+};
 
 type Props = {
   recipes: RecipeProps[];
-}
+};
 
 export default function Home(props: Props) {
   console.log({ props });
 
   return (
-    <div className="min-h-screen">
-      <Card
-        my={"7"}
-        variant="classic"
-        size={"4"}
-        style={{ borderRadius: "0" }}
-      >
-        <Container py={"6"}>
-          <Flex direction={"column"}>
-            <Heading as="h1" mb={"4"} size={"7"}>
-              Lorem ipsum dolor sit amet, qui minim labore adipisicing minim
-              sint cillum sint consectetur cupidatat.
-            </Heading>
-            <Text as="p" size={"4"}>
-              Qui minim labore adipisicing minim sint
-            </Text>
-            <Box mt={"6"}>
-              <Button size={"3"}>Learn More</Button>
-            </Box>
-          </Flex>
-        </Container>
-      </Card>
-    </div >
+    <Flex direction="column" gap="5" className="min-h-screen p-5">
+      {props.recipes.map((recipe, index) => (
+        <Recipe key={index} recipe={recipe} />
+      ))}
+    </Flex>
   );
 }
