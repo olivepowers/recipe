751f2e36532879be8a6c9ec56be8263a3b6dee1a
olivepowers <powersog@gmail.com>
2023-10-10 09:39:18 -0700

    show list specific recipes

--- src/pages/myrecipes.tsx
+++ src/pages/myrecipes.tsx
@@ -1,13 +1,14 @@
-import { Button, Flex } from "@radix-ui/themes";
+import { Button, Flex, Separator, Text } from "@radix-ui/themes";
 import type { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import prisma from "../lib/prisma";
 import RecipeComponent from "@web/components/RecipeComponent";
 import Layout from "@web/components/Layout";
 import AddRecipeModal from "@web/components/AddRecipeModal";
-import { Recipe } from "@prisma/client";
+import { List, Recipe, ListRecipe } from "@prisma/client";
 import Sidebar from "@web/components/Sidebar";
-import { useState } from "react";
+import { Fragment, useState } from "react";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 const getHashtags = (hashtagStr?: string) => {
   const hashtags = hashtagStr ? hashtagStr.split(",") : [];
@@ -17,6 +18,8 @@ const getHashtags = (hashtagStr?: string) => {
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
   const session = await getSession(context); // Get the session data
   const hashtags = getHashtags(context.query?.hashtags);
+  const listTab = context.query?.list;
+  console.log(listTab);
 
   let where: {
     author: { email: string | null };
@@ -54,19 +57,31 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
       }, []);
       return Array.from(new Set(allRecipeHashtags));
     });
+
+  const lists = await prisma.list.findMany({
+    where: {
+      author: {
+        email: session?.user?.email || null,
+      },
+    },
+  });
+
   return {
-    props: { recipes, recipeHashtags },
+    props: { recipes, recipeHashtags, lists, listTab },
   };
 };
 
 type Props = {
   recipes: Recipe[];
   recipeHashtags: string[];
+  lists: List[];
+  listTab: string;
 };
 
 export default function MyRecipes(props: Props) {
-  console.log({ props });
+  console.log("props", { props });
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [selectedList, setSelectedList] = useState<List | null>();
 
   return (
     <Layout>
@@ -74,6 +89,30 @@ export default function MyRecipes(props: Props) {
       <div className="flex">
         <Sidebar hashtags={props.recipeHashtags} />
         <div className="flex-1 p-5">
+          <Text size="2">
+            <Flex
+              gap="3"
+              align="center"
+              // className={`${
+              //   selectedList === "My Recipes" ? "text-blue-500" : ""
+              // }cursor-pointer`}
+            >
+              <Button variant="ghost" onClick={() => setSelectedList(null)}>
+                My Recipes
+              </Button>
+              {props.lists.map((list, index) => (
+                <Fragment key={index}>
+                  <Separator orientation="vertical" />
+                  <Button variant="ghost" onClick={() => setSelectedList(list)}>
+                    {list.name}
+                  </Button>
+                </Fragment>
+              ))}
+              <Separator orientation="vertical" />
+              <PlusIcon />
+            </Flex>
+            <Separator size="4" orientation="horizontal" />
+          </Text>
           <Flex className="min-h-screen p-5 flex-col">
             <Flex justify="end" p="2">
               <Button onClick={() => setIsAddModalOpen(true)}>
@@ -81,9 +120,22 @@ export default function MyRecipes(props: Props) {
               </Button>
             </Flex>
             <Flex direction="column" gap="5">
-              {props.recipes.map((recipe, index) => (
-                <RecipeComponent key={index} recipe={recipe} />
-              ))}
+              {selectedList
+                ? props.recipes
+                    .filter((recipe) =>
+                      recipe.lists.some(
+                        (list: ListRecipe) => list.listId === selectedList.id
+                      )
+                    )
+                    .map((recipe, index) => (
+                      <RecipeComponent key={index} recipe={recipe} />
+                    ))
+                : props.recipes.map((recipe, index) => (
+                    <RecipeComponent key={index} recipe={recipe} />
+                  ))}
+              {/* {props.recipes.map((recipe, index) => ( */}
+              {/* <RecipeComponent key={index} recipe={recipe} /> */}
+              {/* ))} */}
             </Flex>
           </Flex>
         </div>
