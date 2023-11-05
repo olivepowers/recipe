2e5dc8a0f65abcbdc0ac251fba9b7b7bfeb83df1
olivepowers <powersog@gmail.com>
2023-11-05 15:49:02 -0500

    add ?pgbouncer=true suffix to db url

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,11 @@
 import { PrismaClient, Recipe } from "@prisma/client";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL + "?pgbouncer=true",
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
